8518c970fe228a5124d50fc634218912dc392955
Gabriel Souza <gabriel.souza@elgin.com.br>
2024-10-10 12:07:55 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/gabs1337/gopportunities/router"
+import (
+	"github.com/gabs1337/gopportunities/config"
+	"github.com/gabs1337/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
