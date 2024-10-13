5e604170d92fddb03c5884ee003e9a5a67afd368
Gab <gabriel.souza@elgin.com.br>
2024-10-13 15:42:28 -0400

    feat: list all openings handler

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,16 @@ package handler
 import (
 	"net/http"
 
+	"github.com/gabs1337/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
 }
